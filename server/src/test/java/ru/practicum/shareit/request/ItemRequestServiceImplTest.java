bb5a94b1786da42a7fd8950916bcc5264687247f
vkuts <Viktor Kutsevalov>
2026-08-11 23:10:26 +0300

    ТЗ спринта 16. Исправление замечаний

--- server/src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.model.ItemRequest;
@@ -19,7 +18,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
